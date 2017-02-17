1e38b24816792612905b67f66cd79d79d058d6a3
soichit <soichit@uw.edu>
2017-02-17 00:21:02 -0800

    still parsing

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,23 +10,29 @@ using System.Net;
 using System.IO;
 using System.Collections.Generic;
 using System.Xml;
+using System.Linq;
 
 namespace ConsoleApplication1
 {
     class Program
     {
-        private static CloudQueue xmlQueue;
-        private static CloudQueue htmlQueue;
+        //private static CloudQueue xmlQueue;
+       /private static CloudQueue htmlQueue;
+        private static List<String> xmlList;
         private static CloudTable table;
+        private static DateTime cutoffDate;
 
         static void Main(string[] args)
         {
             CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
                  CloudConfigurationManager.GetSetting("StorageConnectionString"));
             CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
-            xmlQueue = queueClient.GetQueueReference("myXml");
-            htmlQueue = queueClient.GetQueueReference("myHtml");
 
+            //xmlQueue = queueClient.GetQueueReference("myxml");
+            //xmlQueue.CreateIfNotExists();
+            List<String> xmlList = new List<String>();
+            htmlQueue = queueClient.GetQueueReference("myhtml");
+            htmlQueue.CreateIfNotExists();
 
             //CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
             //table = tableClient.GetTableReference("sum");
@@ -38,28 +44,52 @@ namespace ConsoleApplication1
             //table.Execute(insertOperation);
 
 
-            CloudQueueMessage message;
-            string url = "http://www.cnn.com/robots.txt";
+            Console.WriteLine("1");
+            cutoffDate = new DateTime(2016, 12, 1); // 12/1/2016
+
             //parseRobot(url);
-            //crawlPage(url);
-            parseXML("http://www.cnn.com/sitemaps/sitemap-index.xml");
-            do
+            parseHTML("http://www.cnn.com/");
+            parseHTML("http://www.cnn.com/2017/02/16/us/museum-removes-art-from-immigrants-trnd");
+
+
+            string url = "http://www.cnn.com/robots.txt";
+            CloudQueueMessage message = new CloudQueueMessage("");
+            //CloudQueueMessage message2 = new CloudQueueMessage("");
+
+
+            while (message != null && xmlList.Any())
             {
-                message = xmlQueue.GetMessage(TimeSpan.FromMinutes(5));
-                xmlQueue.DeleteMessage(message);
-                Console.WriteLine(message.AsString);
-                parseXML(message.AsString);
-            } while (message != null);
+                // parse xml
+                if (xmlList.Any())
+                {
+                    var lastItem = xmlList.Last();
+                    xmlList.Remove(xmlList.Last());
+                    parseXML(lastItem);
+                }
+                // parse html
+                else
+                {
+                    message = htmlQueue.GetMessage(TimeSpan.FromMinutes(5));
+                    
+                    if (message != null)
+                    {
+                        //Console.WriteLine(message.AsString);
+                        htmlQueue.DeleteMessage(message);
+                        parseHTML(message.AsString);
+                    }
+                }
+            }
 
             Console.WriteLine("done");
             Console.ReadLine();
         }
 
-        public static void crawlPage(String url)
+
+        public static void parseHTML(string link)
         {
             // web crawler
             HtmlWeb web = new HtmlWeb();
-            HtmlDocument htmlDoc = web.Load(url);
+            HtmlDocument htmlDoc = web.Load(link);
             Console.WriteLine("1");
 
             // ParseErrors is an ArrayList containing any errors from the Load statement
@@ -67,20 +97,25 @@ namespace ConsoleApplication1
             if (htmlDoc.DocumentNode != null)
             {
                 HtmlAgilityPack.HtmlNode bodyNode = htmlDoc.DocumentNode.SelectSingleNode("//body");
-                if (bodyNode != null)
-                {
-                    //Console.WriteLine(bodyNode.InnerText);
-                }
+                string title = "" + htmlDoc.DocumentNode.SelectSingleNode("//head/title");
+                //if (bodyNode != null)
+                // insert webpage into table
+                //Webpage w = new Webpage(link, title, "" + bodyNode);
+                //TableOperation insertOperation = TableOperation.Insert(w);
+                //table.Execute(insertOperation);
             }
-
-            //HtmlNode[] nodes = htmlDoc.DocumentNode.SelectNodes("//a").ToArray();
-            //foreach (HtmlNode item in nodes)
-            //{
-            //    Console.WriteLine(item.InnerHtml);
-            //}
-            Console.WriteLine("done");
-            Console.ReadLine();
+            HtmlNode[] nodes = htmlDoc.DocumentNode.SelectNodes("//a[@href]").ToArray();
+            foreach (HtmlNode item in nodes)
+            {
+                // insert into Queue
+                //CloudQueueMessage url = new CloudQueueMessage("" + item);
+                string hrefValue = item.GetAttributeValue("href", string.Empty);
+                Console.WriteLine(hrefValue);
+                //Console.WriteLine(item.InnerHtml);
+                //queue.AddMessage(url);
+            }
         }
+
 
         public static void parseRobot(string url)
         {
@@ -146,15 +181,16 @@ namespace ConsoleApplication1
                             if (link.Substring(link.Length - 4) == ".xml")
                             {
                                 // add to xml queue
-                                Console.WriteLine(reader.Value);
-                                CloudQueueMessage xmlLink = new CloudQueueMessage(reader.Value);
-                                xmlQueue.AddMessage(xmlLink); //storage error?
+                                //Console.WriteLine(reader.Value);
+                                //CloudQueueMessage xmlLink = new CloudQueueMessage(reader.Value);
+                                //xmlQueue.AddMessage(xmlLink);
+                                xmlList.Add(reader.Value); //cast to string?
                             } else if (link.Substring(link.Length - 5) == ".html")
                             {
                                 // add to url queue
-                                Console.WriteLine(reader.Value);
+                                //Console.WriteLine(reader.Value);
                                 CloudQueueMessage htmlLink = new CloudQueueMessage(reader.Value);
-                                xmlQueue.AddMessage(htmlLink);
+                                htmlQueue.AddMessage(htmlLink);
                             }                
                         }
                         break;
